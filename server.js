542bdc0335b710fca11862445eb3cab9b65598f3
thandon263 <ncubethando20@gmail.com>
2016-11-21 22:12:51 -0500

    Creating express template, for my storm trooper app

---
+++ server.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+var express = require('express');
+var stormpath = require('express-stormpath');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(stormpath.init(app, {
+  web: {
+    produces: ['application/json']
+  }
+}));
+
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err){
+    if(err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+});
